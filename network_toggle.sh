0a8b9be92253e8f3553f6bff87bfbe8cce5ea541
denny <zhaihui19881102@163.com>
2019-09-26 21:27:53 -0400

    ssid and freq and the latency is ok now

--- network_toggle.sh
+++ network_toggle.sh
@@ -4,7 +4,7 @@
 ip_server=http://www.baidu.com
 ip_server=http://www.mi.com
 #ip_server=10.18.103.150
-ip_server=172.31.255.254
+#ip_server=172.31.255.254
 dev_phy=wlxec3dfdfa4cbb
 latency_ok=4000
 signal_intensity_ok=70
@@ -16,15 +16,18 @@ for i in $(seq 1 200)
 do
     sleep 3;
 
-    httping -c1 -g $ip_server >/home/denny/test/latency.txt
+    httping -c1 -g $ip_server >/home/denny/git_file/robot_script/latency.txt
 
 
-    iw dev $dev_phy link > /home/denny/test/link_info.txt
+    iw dev $dev_phy link > /home/denny/git_file/robot_script/link_info.txt
     #sig_intensity='grep "signal*" link_info.txt'
 
     sig_intensity=$(cat link_info.txt | grep "sign*"|cut -c11-12);
-    bssid=$(cat link_info.txt | grep "Connected*"|cut -c14-31);
-    latency=$(cat latency.txt | grep "connected*"|cut -c56-57);
+    bssid=$(cat link_info.txt | grep "Connected*"|cut -c20-31);
+    ssid=$(cat link_info.txt | grep "SSID*"|cut -c7-25);
+    freq=$(cat link_info.txt | grep "freq*"|cut -c7-15);
+    latency=$(cat latency.txt | grep "time=*"|cut -c56-57);
+    latency=$(cat latency.txt | grep "time=*"|cut -c56-60|cut -d\. -f 1);
 
   #  wifi_quality_1=$(($w_signal_intensity*$sig_intensity))
   #  wifi_quality_2=$(($w_latency*$latency))
@@ -48,9 +51,9 @@ do
         sleep 12    ;
                
     else if [ $sig_intensity -lt $signal_intensity_ok ]; then
-        echo "BSSID is " $bssid"  signal intensity is " $sig_intensity  "  latency is " $latency  "   network is ok";
+        echo "SSID:"$ssid "  freq:" $freq" BSSID:" $bssid"  signal intensity:" $sig_intensity  "  latency:" $latency  "  network is ok" ;
     else
-        echo "BSSID is " $bssid"   signal intensity is " $sig_intensity "  latency is " $latency"   network is bad";
+        echo "SSID:"$ssid "  freq:" $freq" BSSID:" $bssid"  signal intensity:" $sig_intensity  "  latency:" $latency  "  network is bad" ;
         echo "reconnecting";
         sudo iw dev $dev_phy disconnect;
         sleep 12;
